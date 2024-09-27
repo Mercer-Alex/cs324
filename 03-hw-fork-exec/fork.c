bf7f129093f62fe2d1b43b942d3eb2f4faff269a
Alexandria Isabella Mercer <mercera@wyoming.cs.byu.edu>
2024-09-27 12:04:37 -0600

    pls work

--- 03-hw-fork-exec/fork.c
+++ 03-hw-fork-exec/fork.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
 	int pid;
+	int pipefd[2];
 
 	printf("Starting program; process has pid %d\n", getpid());
+
+	FILE *fileName = fopen("fork-output.txt", "w+");
+
+	fprintf(fileName, "BEFORE FORK (%d)\n", fileno(fileName));
+	fflush(fileName);
+
+	if (pipe(pipefd) == -1) {
+		perror("pipe failed");
+		exit(1);
+	}
 
 	if ((pid = fork()) < 0) {
 		fprintf(stderr, "Could not fork()");
@@ -15,26 +27,45 @@ int main(int argc, char *argv[]) {
 	/* BEGIN SECTION A */
 
 	printf("Section A;  pid %d\n", getpid());
-	sleep(5);
+	//sleep(5);
 
 	/* END SECTION A */
 	if (pid == 0) {
 		/* BEGIN SECTION B */
-
-		printf("Section B\n");
-		sleep(30);
-		printf("Section B done sleeping\n");
-
+		
+		char *newenviron[] = { NULL };
+		printf("Program \"%s\" has pid %d. Sleeping.\n", argv[0], getpid());
+						
+		if (argc <= 1) {
+			printf("No program to exec.  Exiting...\n");
+			exit(0);
+		}
+		
+		printf("Running exec of \"%s\"\n", argv[1]);
+		execve(argv[1], &argv[1], newenviron);
+		printf("End of program \"%s\".\n", argv[0]);
 		exit(0);
 
-		/* END SECTION B */
 	} else {
 		/* BEGIN SECTION C */
-
+		fprintf(fileName, "SECTION C (%d)\n", fileno(fileName));
+		close(pipefd[1]);
+		char buffer[128];
+		ssize_t bytesRead = read(pipefd[0], buffer, sizeof(buffer) -1);
+		
+		if (bytesRead > 0) {
+		       	buffer[bytesRead] = '\0';
+		       	printf("Read %ld bytes from pipe: %s", bytesRead, buffer);
+	       	} else {
+			printf("No data read from pipe\n");
+		}
+	
+		close(pipefd[0]);
 		printf("Section C\n");
-		sleep(60);
-		printf("Section C done sleeping\n");
-
+		wait(NULL);
+		//sleep(30);
+		//printf("Section C done sleeping\n");
+		fclose(fileName);
 		exit(0);
 
 		/* END SECTION C */
@@ -46,4 +77,5 @@ int main(int argc, char *argv[]) {
 
 	/* END SECTION D */
 }
+
 
